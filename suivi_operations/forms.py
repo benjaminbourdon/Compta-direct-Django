9f99c70fbe77f0447a809722425c6f318bb24747
Benjamin B <dev@benjamin-b.fr>
2023-06-21 16:57:34 +0200

    Manage list of members import from file

--- suivi_operations/forms.py
+++ suivi_operations/forms.py
@@ -1,4 +1,9 @@
+import csv
+import io
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -13,5 +18,28 @@ class ImportFileForm(forms.Form):
         required=True,
         allow_empty_file=False,
         widget=forms.FileInput,
+        validators=[FileExtensionValidator(["csv"])],
+        help_text=_("File must be CSV text type."),
     )
     category = forms.ChoiceField(label=_("file category"), choices=CategoryFile.choices)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if "file" in cleaned_data:
+            file = cleaned_data["file"].read()
+            try:
+                decoded_file = file.decode("utf-8")
+            except UnicodeDecodeError:
+                # Could be a validator or extend to more encodings
+                raise ValidationError(_("File must be encode with UTF-8."))
+            else:
+                cleaned_data["file"] = decoded_file
+                read_csv = csv.DictReader(io.StringIO(decoded_file))
+
+                if cleaned_data["category"] == ImportFileForm.CategoryFile.MEMBER_LIST:
+                    if "Email" not in read_csv.fieldnames:
+                        raise ValidationError(
+                            _('CSV file doesn\'t have an "Email" colomn.')
+                        )
+        return cleaned_data
--- suivi_operations/views.py
+++ suivi_operations/views.py
@@ -1,8 +1,16 @@
+import csv
+import io
+import re
+from datetime import datetime
+
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.edit import FormView
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 from .forms import ImportFileForm
+from .models import User
 
 
 class ImportFileView(SuccessMessageMixin, FormView):
@@ -10,3 +18,49 @@ class ImportFileView(SuccessMessageMixin, FormView):
     template_name = "importfile.html"
     success_url = "#"
     success_message = _("Imported successfully")
+
+    def form_valid(self, form):
+        read_csv = csv.DictReader(io.StringIO(form.cleaned_data["file"]))
+
+        if form.cleaned_data["category"] == ImportFileForm.CategoryFile.MEMBER_LIST:
+            count_new_members = 0
+            for line in read_csv:
+                try:
+                    validate_email(line["Email"])
+                except ValidationError:
+                    pass
+                else:
+                    user_values = {
+                        "first_name": line["Prénom"],
+                        "last_name": line["Nom"],
+                        "gender": User.Gender.MEN
+                        if line["Sexe"] == "Masculin"
+                        else User.Gender.WOMEN,
+                        "phone_number": line["Téléphone mobile"]
+                        if line["Téléphone mobile"] != ""
+                        else line["Téléphone fixe"],
+                        "date_of_birth": datetime.strptime(
+                            line["Date de naissance"], "%d/%m/%Y"
+                        ),
+                    }
+                    user, is_new = User.objects.update_or_create(
+                        email=line["Email"], defaults=user_values
+                    )
+                    if is_new:
+                        count_new_members += 1
+                        user.is_active = False
+                    user.profile_ac.idContact = line["ID du Contact"]
+                    user.profile_ac.member_revo = (
+                        True
+                        if re.match("Adhérent", line["Statut adhérent"]) is not None
+                        else False
+                    )
+                    user.profile_ac.member_CS = line["Membre annuels (aides CS)"] != ""
+                    user.profile_ac.detail_url = line["Détail"]
+                    user.profile_ac.last_check = datetime.today()
+
+                    user.save()
+
+            self.success_message += f" ({count_new_members} member(s) added)"
+
+        return super().form_valid(form)
