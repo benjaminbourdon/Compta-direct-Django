1f42cf7e5926bfd15c5853d21e44920706dfe4d1
Benjamin B <dev@benjamin-b.fr>
2023-06-21 03:01:54 +0200

    Add suivi_operations to installed app

--- intranet_compta/settings.py
+++ intranet_compta/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "suivi_operations.apps.SuiviOperationsConfig",
 ]
 
 MIDDLEWARE = [
